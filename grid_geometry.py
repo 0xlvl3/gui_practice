8856f5954d53cf2dbeeb650bba20282f2285d95b
0xlvl3 <cryptochoccy@gmail.com>
2023-01-05 10:21:31 +1100

    More widget practice; advanced widgets

--- grid_geometry.py
+++ grid_geometry.py
@@ -10,7 +10,7 @@ These are the position of each widget relative to other widgets.
 
 """
 root = Tk()
-content = ttk.Frame(root)
+content = ttk.Frame(root, padding=(3, 3, 12, 12))
 frame = Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
 namelbl = ttk.Label(content, text="Name")
 name = ttk.Entry(content)
@@ -23,7 +23,7 @@ one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
 two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
 three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
 ok = ttk.Button(content, text="Okay")
-cancel = ttk.Button(content, text="Cancel", command=exit)
+cancel = ttk.Button(content, text="Cancel")
 
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
---
+++ more_widgets.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+# https://tkdocs.com/tutorial/morewidgets.html
+# ListBox
+
+root = Tk()
+frame = Frame(root, background="blue", width=500, height=750).grid()
+
+root.mainloop()
