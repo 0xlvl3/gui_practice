b07caad3613af50af6a0019ee6e53451c9410919
0xlvl3 <cryptochoccy@gmail.com>
2023-01-05 14:05:30 +1100

    Work on changing frame tomorrow

--- test/customer_test.py
+++ test/customer_test.py
@@ -4,15 +4,21 @@ customtkinter.set_appearance_mode("dark")  # System theme for our application
 customtkinter.set_default_color_theme("dark-blue")  # Color theme for our application
 
 root = customtkinter.CTk()
-root.geometry("500x350")
+root.geometry("750x530")
+root.title("Flashcards")
 
 
 def login():
     print("test")
 
 
+# login menu
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+# after login
+frame2 = customtkinter.CTkFrame(master=root)
+frame.pack()
 
 label = customtkinter.CTkLabel(
     master=frame,
@@ -32,4 +38,12 @@ button.pack(pady=12, padx=10)
 
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
 checkbox.pack(pady=12, padx=10)
+
+dropdown = customtkinter.CTkComboBox(
+    master=frame, values=["options1", "options2", "options3"]
+)
+dropdown.pack(padx=20, pady=10)
+dropdown.set("options1")
+
+
 root.mainloop()
